727215841b4f75f60ba5534284fec235e80a881f
Ana Maria <gomitaana@hotmail.com>
2016-11-18 19:29:17 -0600

    First form

--- digitRecognition/src/digitrecognition/DigitRecognition.java
+++ digitRecognition/src/digitrecognition/DigitRecognition.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package digitrecognition;
+import static java.awt.Frame.MAXIMIZED_BOTH;
 
 /**
  *
  * @author anamariagonzalez
  */
 public class DigitRecognition {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        sudokuBoard board = new sudokuBoard();
+        board.setExtendedState(MAXIMIZED_BOTH); 
+        board.setVisible(true);
     }
     
 }
