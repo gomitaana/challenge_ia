f958978b53af865ef4a1c18f131eeb4777190f93
rulgf <rulgf.420@gmail.com>
2016-11-20 13:44:34 -0600

    Sudoku solver un link no feik maton alfa 3.0 :v

--- sudoku2/src/sudoku2/main.java
+++ sudoku2/src/sudoku2/main.java
@@ -14,7 +14,16 @@ public class main {
 			{ 0, 0, 8, 0, 0, 0, 2, 0, 0 }, 
 			{ 5, 0, 0, 6, 0, 8, 0, 0, 1 } };
 	
-	public node[][] m_nodes;
+	/*static int sudoku[][] = { 
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+			{ 0, 0, 0, 1, 6, 4, 8, 2, 7 }, 
+			{ 0, 0, 0, 3, 8, 7, 5, 9, 1 }, 
+			{ 4, 1, 2, 8, 9, 6, 7, 3, 5 }, 
+			{ 5, 7, 3, 2, 4, 1, 6, 8, 9 }, 
+			{ 8, 9, 6, 7, 5, 3, 1, 4, 2 }, 
+			{ 9, 3, 8, 4, 1, 5, 2, 7, 6 }, 
+			{ 1, 4, 7, 6, 3, 2, 9, 5, 8 }, 
+			{ 2, 6, 5, 9, 7, 8, 4, 1, 3 } };*/
 	
 	public List<Integer> initializeValues(node Node){
 		List<Integer> values = new ArrayList<Integer>();
@@ -45,26 +54,111 @@ public class main {
 				}
 			}
 		}
-		
 		return values;
 	}
 	
-	public void initializeNodes(){
-		for(int i =0; i< sudoku.length; i++){
-			for(int j =0; j< sudoku[i].length; j++){
-				if(sudoku[i][j] > 0){
-					node n_node = new node(i, j);
-					List<Integer> possible =this.initializeValues(n_node);
-					boolean update = n_node.updateValues(possible);
-					this.m_nodes[i][j] = n_node;
-				}else{
-					m_nodes[i][j] = null;
+	public boolean resolveSudoku(){
+		
+		node[][] mx_nodes = this.getMatrix();
+		List<node> l_nodes= this.getPossibleN(mx_nodes);
+		
+		//If the list of nodes is empty we have reached the end
+		if(l_nodes.isEmpty()){
+			return true;
+		}
+		
+		//Initialize Values
+		for(int i =0; i<l_nodes.size(); i++){
+			List<Integer> l_possibles = this.initializeValues(l_nodes.get(i));
+			boolean update = l_nodes.get(i).updateValues(l_possibles);
+			if(update == false){
+				return false;
+			}
+		}
+		
+		//Sort based in the possible attribute and Get first node
+		Collections.sort(l_nodes);
+		node actual = l_nodes.get(0);
+		//System.out.println("Actual: "+actual.row + " " + actual.col);
+		
+		//Get possible values of actual node
+		List<Integer> n_possibles = actual.possibleValues();
+		//System.out.println("Valores: "+ n_possibles);
+		
+		int i=0;
+		while(i < n_possibles.size()){
+			sudoku[actual.row][actual.col] = n_possibles.get(i);
+			/*for (int row = 0; row < 9; row++) {
+				for (int col = 0; col < 9; col++){
+					System.out.print(sudoku[row][col]);
+				}
+				System.out.println();
+		 	}
+			System.out.println();*/
+			
+			//Recursion for the rest of the sudoku
+			boolean next = this.resolveSudoku();
+			if(next == false){
+				i++;
+			}else{
+				return true;
+			}
+		}
+		return false;
+		
+	}
+	
+	public List<node> getPossibleN(node[][] m_nodes){
+		List<node> p_nodes = new ArrayList<node>();
+		for(int i =0; i< m_nodes.length; i++){
+			for(int j =0; j< m_nodes[i].length; j++){
+				if(m_nodes[i][j] != null){
+					p_nodes.add(m_nodes[i][j]);
 				}
 			}
 		}
+		return p_nodes;
+	}
+	
+	public node[][] getMatrix(){
+		node[][] mNodes = new node[9][9];
+		for(int i =0; i< sudoku.length; i++){
+			for(int j =0; j< sudoku[i].length; j++){
+				if(sudoku[i][j] == 0){
+					node n_node = new node(i, j);
+					//List<Integer> possible =this.initializeValues(n_node);
+					//boolean update = n_node.updateValues(possible);
+					mNodes[i][j] = n_node;
+				}else{
+					mNodes[i][j] = null;
+				}
+			}
+		}
+		return mNodes;
 	}
 	
 	public static void main(String[] args) {
+		// this function is called just once, and it call the InitializeValues function for each node
+		//It initialize the matrix of nodes, and the array of nodes we will be iterating
 		
+		//Once initialized execute the algorithm
+		//1. order based in possible values (less to more)
+		//2. assign value for first node, and update the rest
+		//3. repeat steps for next node
+		//Constraints: if in the update, one node or more have no more possible answers return to previous node
+		
+		boolean resolve = new main().resolveSudoku();
+		if(resolve){
+			System.out.println("Resolved: ");
+			
+			for (int row = 0; row < 9; row++) {
+				for (int col = 0; col < 9; col++){
+					System.out.print(sudoku[row][col]);
+				}
+				System.out.println();
+		 	}
+		}else{
+			System.out.println("The sudoku cannot be resolved");
+		}
 	}
 }
--- sudoku2/src/sudoku2/node.java
+++ sudoku2/src/sudoku2/node.java
@@ -3,15 +3,17 @@ package sudoku2;
 import java.util.*;
 import java.util.stream.*;
 
-public class node {
+public class node implements Comparable<node> {
 	//Dimensions of the node
 	int row;
 	int col;
 	int values[]={0,0,0,0,0,0,0,0,0};
+	int total_posible; 
 	
 	public node(int row, int col){
 		this.row = row;
 		this.col = col;
+		this.total_posible = 9;
 	}
 	
 	public void printNode(){
@@ -28,6 +30,14 @@ public class node {
 			this.values[value_asg.get(i)-1] = 1;
 		}
 		
+		//Update total_possible
+		this.total_posible =0;
+		for(int i =0; i<this.values.length; i++){
+			if(this.values[i]== 0){
+				this.total_posible += 1;
+			}
+		}
+		
 		//check if it has possible values
 		boolean contains = IntStream.of(this.values).anyMatch(x -> x == 0);
 		
@@ -42,5 +52,41 @@ public class node {
 	
 	public int[] getValues(){
 		return this.values;
+	}
+	
+	public int compareTo(node N)
+	{
+	     return(this.total_posible - N.total_posible);
+	}
+	
+	public List<Integer> possibleValues(){
+		List<Integer> possible = new ArrayList<Integer>();
+		for(int i =0; i <this.values.length; i++){
+			if(this.values[i] == 0){
+				possible.add(i+1);
+			}
+		}
+		//System.out.println("Possible:"+possible);
+		return possible;
+	}
+
+	public boolean updateValue(int value){
+		this.values[value-1]=1;
+		
+		for(int i =0; i<this.values.length; i++){
+			if(this.values[i]==0){
+				return true;
+			}
+		}
+		
+		this.values[value-1]=0;
+		return false;
+	}
+
+	public void printValues(){
+		for(int i = 0; i< this.values.length; i++){
+			System.out.print(this.values[i]);
+		}
+		System.out.println("");
 	}
 }
